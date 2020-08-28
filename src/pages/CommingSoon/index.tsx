6c0468f90709b9064dae287b5f1af8b40f4dc32d
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-08-28 00:46:15 -0300

    Criação de rotas

---
+++ src/pages/CommingSoon/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const CommingSoon: React.FC = () => {
+  return (
+    <Container>
+      <Text>CommingSoon</Text>
+    </Container>
+  );
+};
+
+export default CommingSoon;
